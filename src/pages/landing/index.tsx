7695554e5c4c0845401c47bb16e584a18d5f3300
Damiao <tower_gamer@hotmail.com>
2021-09-18 15:42:09 -0300

    Customize landing 1

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,9 +1,26 @@
 import type { NextPage } from 'next'
+import styles from './styles.module.scss';
+import Image from 'next/image';
 
 
 const Landing: NextPage = () => {
   return (
-    <h1>Landing</h1>
+    <div className={styles.landingContainer}>
+      <aside>
+        <Image
+            src="/badaman_paper_boat.svg"
+            alt="DCAlogo"
+            height={100}
+            width={100}
+            layout='responsive'
+        />
+        <strong>Diga adeus aos papéis soltos.</strong>
+        <p>Plataforma para armazenamento de documentos para a administração pública.</p>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   )
 }
 
