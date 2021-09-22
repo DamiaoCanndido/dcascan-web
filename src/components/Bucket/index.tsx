57b71e63a54a9b89dd13347225a485b21de8a65e
Damiao <tower_gamer@hotmail.com>
2021-09-22 07:34:46 -0300

    Customize folder and file components 4

--- src/components/Bucket/index.tsx
+++ src/components/Bucket/index.tsx
@@ -1,9 +1,7 @@
 import styles from './styles.module.scss';
 import Link from 'next/link';
 import { bucketProps } from '../../protocols/protocols';
-import Folder from '../Folder';
 import { AiFillFolder, AiFillFilePdf } from "react-icons/ai";
-import File from '../File';
 
 
 export default function Bucket({ buckets }: bucketProps) {
@@ -62,44 +60,3 @@ export default function Bucket({ buckets }: bucketProps) {
     )
 }
 
-/*
-                    {buckets.map(bucket => {
-                        if (bucket.file === undefined) {
-                            return (
-                                <Link href='#'>
-                                    <a>
-                                        <li key={bucket.id}>
-                                            <Folder 
-                                                id={bucket.id} 
-                                                owner={bucket.owner} 
-                                                created_at={bucket.created_at} 
-                                                updated_at={bucket.updated_at} 
-                                                name={bucket.name}
-                                                root={bucket.root}
-                                            />
-                                        </li>
-                                    </a>
-                                </Link>
-                            )
-                        } else {
-                            return (
-                                <Link href='#'>
-                                    <a>
-                                        <li key={bucket.id}>
-                                            <File 
-                                                id={bucket.id} 
-                                                owner={bucket.owner} 
-                                                created_at={bucket.created_at} 
-                                                updated_at={bucket.updated_at} 
-                                                name={bucket.name}
-                                                file={bucket.file}
-                                                key={bucket.key}
-                                                folder={bucket.folder}
-                                            />
-                                        </li>
-                                    </a>
-                                </Link>
-                            )
-                        }
-                    })}
-*/
