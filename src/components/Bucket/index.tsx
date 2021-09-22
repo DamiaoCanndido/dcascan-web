3c08bb77d2db5f38247f8d0945cca4467b731bb5
Damiao <tower_gamer@hotmail.com>
2021-09-21 22:31:11 -0300

    Customize folder and file components 3

--- src/components/Bucket/index.tsx
+++ src/components/Bucket/index.tsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss';
 import Link from 'next/link';
 import { bucketProps } from '../../protocols/protocols';
 import Folder from '../Folder';
+import { AiFillFolder, AiFillFilePdf } from "react-icons/ai";
 import File from '../File';
 
 
@@ -9,7 +10,59 @@ export default function Bucket({ buckets }: bucketProps) {
     return (
         <div className={styles.homePage}>
             <section className={styles.myBucket}>
-                <ul>
+                <table cellSpacing={0}>
+                    <thead>
+                        <th></th>
+                        <th>Nome</th>
+                        <th>Modificado em</th>
+                        <th>Criado em</th>
+                    </thead>
+                    <tbody>
+                        {buckets.map(bucket => {
+                            if (bucket.file === undefined) {
+                                return (
+                                    <tr key={bucket.id}>
+                                        <td>
+                                            <AiFillFolder size='3rem' color='var(--folder)'/>
+                                        </td>
+                                        <td>
+                                            <Link href='#'>
+                                                <a>
+                                                {bucket.name}
+                                                </a>
+                                            </Link>
+                                        </td>
+                                        <td>{bucket.updated_at}</td>
+                                        <td>{bucket.created_at}</td>
+                                    </tr>   
+                                )
+                            } else {
+                                return (
+                                    <tr key={bucket.id}>
+                                        <td>
+                                            <AiFillFilePdf size='3rem' color='var(--pdf)'/>
+                                        </td>
+                                        <td>
+                                            <Link href='#'>
+                                                <a>
+                                                    {bucket.name}
+                                                </a>
+                                            </Link>
+                                        </td>
+                                        <td>{bucket.updated_at}</td>
+                                        <td>{bucket.created_at}</td>
+                                    </tr>  
+                                )
+                            }
+                        })}
+                    </tbody>
+                </table>
+            </section>
+        </div>
+    )
+}
+
+/*
                     {buckets.map(bucket => {
                         if (bucket.file === undefined) {
                             return (
@@ -49,8 +102,4 @@ export default function Bucket({ buckets }: bucketProps) {
                             )
                         }
                     })}
-                </ul>
-            </section>
-        </div>
-    )
-}
+*/
