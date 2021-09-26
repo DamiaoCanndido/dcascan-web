feebcb3538bfcc73ffbaa55e2e16c999d52f6dd6
Damiao <tower_gamer@hotmail.com>
2021-09-26 15:31:53 -0300

    Dropzone 4

--- src/components/SingleFileUploadWithProgress/index.tsx
+++ src/components/SingleFileUploadWithProgress/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 
 export type SingleFileUploadWithProgressProps = {
@@ -7,15 +8,41 @@ export type SingleFileUploadWithProgressProps = {
 
 export function SingleFileUploadWithProgress({file}: SingleFileUploadWithProgressProps){
 
+    const [progress, setProgress] = useState(0);
+    const router = useRouter()
+
     useEffect(() => {
+
         const uploadProcess = async (file: File) => {
             const data = new FormData()
-            //const upfile = await api.post()
+            data.append('file', file)
+            const folder = router.query.uuid as string;
+            if (folder !== undefined) {
+                data.append('folder', folder)
+            }
+            await api.post(
+              'uploads/', 
+              data, {
+                onUploadProgress: (e) => {
+                  const calcProgress = Math.round((e.loaded)*100/e.total)
+                  setProgress(calcProgress)
+                }
+              }
+              ).catch(function(error){
+                  console.log(error)
+              })
+            // if (folder !== undefined) {
+            //   await api.get(`bucket/${folder}`)
+            // } else {
+            //   await api.get('bucket/')
+            // }
         }
         uploadProcess(file);
-    }, [file])
+
+    }, [file, router.query.uuid])
 
     return (
-        <div>SFU</div>
+        <div>SFU - {progress}</div>
     )
 }
+
