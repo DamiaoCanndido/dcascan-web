6bf1a7bfd2c2b3b812efb603d44c3b9ff2fc577e
DamiaoCanndido <tower_gamer@gmail.com>
2021-10-14 21:48:40 -0300

    Move modal 2

--- src/components/MoveModal/index.tsx
+++ src/components/MoveModal/index.tsx
@@ -1,9 +1,25 @@
+import { FormEvent } from 'react';
+import styles from './styles.module.scss';
+
 type modalFunc = {
     modalFunc: () => void;
+    id?: string;
 }
 
-export function MoveModal({ modalFunc }: modalFunc) {
+export function MoveModal({ 
+    modalFunc, 
+    id = 'moveModal' 
+}: modalFunc) {
+
+    async function handleOutSideClick(e: FormEvent) {
+        if ((e.target as Element).id === id) modalFunc() 
+    }
+
     return (
-        <h1>MoveModal</h1>
+        <div id={id} className={styles.shadowZone} onClick={handleOutSideClick}>
+            <div className={styles.modalContainer}>
+
+            </div>
+        </div>
     )
 }
